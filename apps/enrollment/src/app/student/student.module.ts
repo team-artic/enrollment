70d071582543f432441bbf15a826abafa39a2487
Andrés Arias <arias9306@gmail.com>
2020-11-07 21:12:22 -0500

    feat(student): adding the component to list the students

--- apps/enrollment/src/app/student/student.module.ts
+++ apps/enrollment/src/app/student/student.module.ts
@@ -2,9 +2,10 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
 import { StudentRoutingModule } from './student-routing.module';
+import { StudentsComponent } from '../students/students.component';
 
 @NgModule({
-  declarations: [],
+  declarations: [StudentsComponent],
   imports: [CommonModule, StudentRoutingModule],
 })
 export class StudentModule {}
---
+++ apps/enrollment/src/app/students/students.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit, ChangeDetectionStrategy } from '@angular/core';
+
+@Component({
+  selector: 'enrollment-students',
+  templateUrl: './students.component.html',
+  styleUrls: ['./students.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
+})
+export class StudentsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

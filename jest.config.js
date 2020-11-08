303282deaebf91f7ede3952cce55a88212914e23
Andrés Arias <arias9306@gmail.com>
2020-11-07 21:00:16 -0500

    build: add material and ui libs for the angular project

--- jest.config.js
+++ jest.config.js
@@ -1,3 +1,8 @@
 module.exports = {
-  projects: ['<rootDir>/apps/api', '<rootDir>/apps/enrollment'],
+  projects: [
+    '<rootDir>/apps/api',
+    '<rootDir>/apps/enrollment',
+    '<rootDir>/libs/enrollment/material',
+    '<rootDir>/libs/enrollment/ui',
+  ],
 };
---
+++ libs/enrollment/material/README.md
@@ -0,0 +1,7 @@
+# enrollment-material
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test enrollment-material` to execute the unit tests.

6750d50404d67c11c44a4002114224db9e83ef39
Carlos <carloshernando18@gmail.com>
2020-11-07 20:41:08 -0500

    build: create database script

---
+++ database/create-database.sql
@@ -0,0 +1,12 @@
+/*==============================================================*/
+/* Company:        ArticSolutions                               */
+/* DBMS name:      PostgreSQL 9.x                               */
+/* Created on:     07/11/2018 17:53:05 p. m.                    */
+/*==============================================================*/
+CREATE DATABASE "Enrollment"
+  WITH OWNER = postgres
+       ENCODING = 'UTF8'
+       TABLESPACE = pg_default
+       LC_COLLATE = 'Spanish_Spain.1252'
+       LC_CTYPE = 'Spanish_Spain.1252'
+       CONNECTION LIMIT = -1;

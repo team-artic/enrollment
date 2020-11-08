a73c51241fad9742f8a42021c54b1c73b1303455
Andrés Arias <arias9306@gmail.com>
2020-11-07 21:40:47 -0500

    build: configure material components

--- libs/enrollment/material/src/lib/enrollment-material.module.ts
+++ libs/enrollment/material/src/lib/enrollment-material.module.ts
@@ -1,7 +1,31 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FlexLayoutModule } from '@angular/flex-layout';
+import { MatInputModule } from '@angular/material/input';
+import { MatButtonModule } from '@angular/material/button';
+import { MatCardModule } from '@angular/material/card';
+import { MatFormFieldModule } from '@angular/material/form-field';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatTableModule } from '@angular/material/table';
+import { MatIconModule } from '@angular/material/icon';
+import { MatDialogModule } from '@angular/material/dialog';
+import { MatSnackBarModule } from '@angular/material/snack-bar';
+
+const materialModules = [
+  FlexLayoutModule,
+  MatInputModule,
+  MatButtonModule,
+  MatCardModule,
+  MatFormFieldModule,
+  MatToolbarModule,
+  MatTableModule,
+  MatIconModule,
+  MatDialogModule,
+  MatSnackBarModule,
+];
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [CommonModule, ...materialModules],
+  exports: [...materialModules],
 })
 export class EnrollmentMaterialModule {}

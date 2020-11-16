ba247d1a8c57d51d99c5ea7fe59296ced9c980ed
Carlos <carloshernando18@gmail.com>
2020-11-16 16:33:17 -0500

    fix: change nullable property

--- apps/api/src/app/entities/configuration/person.entity.ts
+++ apps/api/src/app/entities/configuration/person.entity.ts
@@ -6,7 +6,7 @@ import {
   JoinColumn,
   ManyToOne,
   PrimaryGeneratedColumn,
-  UpdateDateColumn,
+  UpdateDateColumn
 } from 'typeorm';
 import { List } from './list.entity';
 import { Location } from './location.entity';
@@ -16,74 +16,74 @@ export class Person extends BaseEntity {
   @PrimaryGeneratedColumn({ name: 'Id' })
   id: number;
 
-  @Column({ name: 'TypeIdentification', type: 'int4' })
+  @Column({ name: 'TypeIdentification', nullable: true, type: 'int4' })
   typeIdentification: number;
 
   @ManyToOne(() => List, (list) => list.id)
   @JoinColumn({ name: 'PadreId' })
   typesIdentifications: List;
 
-  @Column({ name: 'Identification', type: 'varchar', length: 30 })
+  @Column({ name: 'Identification', nullable: true, type: 'varchar', length: 30 })
   identification: string;
 
-  @Column({ name: 'FirstName', type: 'varchar', nullable: false, length: 100 })
+  @Column({ name: 'FirstName', type: 'varchar', length: 100 })
   firstName: string;
 
-  @Column({ name: 'SecondName', type: 'varchar', length: 100 })
+  @Column({ name: 'SecondName', nullable: true, type: 'varchar', length: 100 })
   secondName: string;
 
   @Column({ name: 'FirstSurname', nullable: false, length: 100 })
   firstSurname: string;
 
-  @Column({ name: 'SecondSurname', type: 'varchar', length: 100 })
+  @Column({ name: 'SecondSurname', nullable: true, type: 'varchar', length: 100 })
   secondSurname: string;
 
-  @Column({ name: 'BirthDate', type: 'timestamp' })
+  @Column({ name: 'BirthDate', nullable: true, type: 'timestamp' })
   birthDate: string;
 
-  @Column({ name: 'PlaceBirthId', type: 'int4' })
+  @Column({ name: 'PlaceBirthId', nullable: true, type: 'int4' })
   placeBirthId: number;
 
   @ManyToOne(() => Location, (location) => location.id)
   @JoinColumn({ name: 'PlaceBirthId' })
   placeBirth: Location;
 
-  @Column({ name: 'BloodGroupId', type: 'int4' })
+  @Column({ name: 'BloodGroupId', nullable: true, type: 'int4' })
   bloodGroupId: number;
 
   @ManyToOne(() => List, (list) => list.id)
   @JoinColumn({ name: 'BloodGroupId' })
   bloodGroup: List;
 
-  @Column({ name: 'Sisben', type: 'boolean' })
+  @Column({ name: 'Sisben', nullable: true, type: 'boolean' })
   sisben: boolean;
 
-  @Column({ name: 'HealthPromotingCompany', length: 200 })
+  @Column({ name: 'HealthPromotingCompany', nullable: true, length: 200 })
   healthPromotingCompany: string;
 
-  @Column({ name: 'Stratum', type: 'varchar', length: 10 })
+  @Column({ name: 'Stratum', nullable: true, type: 'varchar', length: 10 })
   stratum: string;
 
-  @Column({ name: 'Address', type: 'varchar', length: 200 })
+  @Column({ name: 'Address', nullable: true, type: 'varchar', length: 200 })
   address: string;
 
-  @Column({ name: 'NeighborhoodId', type: 'int4' })
+  @Column({ name: 'NeighborhoodId', nullable: true, type: 'int4' })
   neighborhoodId: number;
 
   @ManyToOne(() => Location, (location) => location.id)
   @JoinColumn({ name: 'NeighborhoodId' })
   neighborhood: Location;
 
-  @Column({ name: 'Phone', type: 'varchar', length: 15 })
+  @Column({ name: 'Phone', nullable: true, type: 'varchar', length: 15 })
   phone: string;
 
-  @Column({ name: 'InstitutionProcedenica', type: 'varchar', length: 200 })
+  @Column({ name: 'InstitutionProcedenica', nullable: true, type: 'varchar', length: 200 })
   institutionProcedenica: string;
 
-  @Column({ name: 'Occupation', type: 'varchar', length: 50 })
+  @Column({ name: 'Occupation', nullable: true, type: 'varchar', length: 50 })
   occupation: string;
 
-  @Column({ name: 'Displaced', type: 'boolean' })
+  @Column({ name: 'Displaced', nullable: true, type: 'boolean' })
   displaced: boolean;
 
   @CreateDateColumn({ name: 'CreatedAt' })
